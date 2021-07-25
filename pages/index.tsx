7f6ec3fa41465d54399b6cd0ba27c574341f91bd
Jyotikanta <jyotikanta.n>
2021-07-25 23:22:40 +0530

    axios and interceptor introduced

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,18 @@ import { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import Link from 'next/link'
 import { startClock } from '../src/web/redux/actions/actions'
-import Examples from '../src/web/components/examples'
+import Examples from '../src/web/components/examples';
+import { axiosInstance } from '../src/web/helper/axios.util';
 
 const Index = () => {
   const dispatch = useDispatch()
   useEffect(() => {
     dispatch(startClock())
   }, [dispatch])
+  useEffect(()=>{
+      let url = '/typicode/demo/posts';
+      axiosInstance.get(url).then(data => {console.log('fetched==>',data);})
+    },[])
 
   return (
     <>
